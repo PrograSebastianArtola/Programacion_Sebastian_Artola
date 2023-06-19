ee0dba1b580eae888e3bb203446dc268209e4f94
PrograSebastianArtola <136514727+PrograSebastianArtola@users.noreply.github.com>
2023-06-19 15:23:53 -0600

    Add files via upload

---
+++ Nombres y espacios.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+
+using namespace std,
+
+int main(){
+	
+	char nombre[20];
+	
+	cout<<"ingrese el nombre de las personas:"<<endl;
+	cin.getline(nombre,20,)
+	
+	cout<<nombre<<endl;
+	
+	int distancia=string(nombre);//para determinar la cantidad de espacios
+	
+	cout<<"cantidad de espacios:"<<distancia;
+	
+}
